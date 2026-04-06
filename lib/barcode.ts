a1c466a671d46a3aa3826fc543b1f2b5b0b561cb
LEONARD KIPCHIRCHIR <109794940+Masterkartel@users.noreply.github.com>
2026-04-06 12:49:04 +0300

    Delete lib/barcode.ts

--- lib/barcode.ts
+++
@@ -1,4 +0,0 @@
-export function barcodePattern(value: string) {
-  const bits = Array.from(value || "").map((ch) => ch.charCodeAt(0).toString(2).padStart(8, "0")).join("");
-  return `1010${bits}11101`;
-}
